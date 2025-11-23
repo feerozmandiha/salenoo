486a5e14c0f9a04ac090444e060dde9ccd8f2ba9
Ahmad feerozmandi <feerozmandiha>
2025-11-23 15:18:56 +0330

    اصلاح هدر

--- assets/js/header/VerticalHeaderAnimator.js
+++ assets/js/header/VerticalHeaderAnimator.js
@@ -517,7 +517,7 @@ class VerticalHeaderAnimator {
             });
         } else if (isScrollingDown && st > threshold) {
             gsap.to(this.header, {
-                y: -headerHeight - 10,
+                y: -headerHeight - 12,
                 duration: 0.4,
                 ease: 'power2.in'
             });
